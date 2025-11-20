34f67208d32c90bb7328e65fc5a06a3d6066e9b5
adilstudentbcn <adil.student.bcn@gmail.com>
2025-11-20 14:37:29 +0100

    home page background  and logo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,41 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Rocket Jobs</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rocket Jobs</title>
 
-  {{-- Use the same Tailwind setup as the rest of the app --}}
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
-  <div class="min-h-screen px-10">
+<body class="bg-black text-white">
+    <div class="min-h-screen px-10">
 
-    <nav class="flex items-baseline justify-between py-4">
-      <div>
-        <a href="/">
-          <img src="{{ Vite::asset('resources/images/logo.svg')}}" alt="logo">
-        </a>
-      </div>
+        <nav class="flex items-baseline justify-between py-4">
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/images/logo.png') }}" 
+     alt="logo" 
+     class="w-12 h-auto">
 
-      <div class="space-x-6">
-        <a href="#"> Jobs link</a>
-        <a href="#"> Careers link </a>
-        <a href="#"> Salaries link</a>
-      </div>
+                </a>
+            </div>
 
-      <div>
-        <a href=""> Post a Job</a>
-      </div>
-    </nav>
+            <div class="space-x-6">
+                <a href="#">Jobs link</a>
+                <a href="#">Careers link</a>
+                <a href="#">Salaries link</a>
+            </div>
 
-    <main>
-      {{ $slot }}
-    </main>
-  </div>
+            <div>
+                <a href="">Post a Job</a>
+            </div>
+        </nav>
 
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
 </body>
-
 </html>
